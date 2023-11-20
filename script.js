44963da5ab6c8a9fc2ae40474c7dee3b74a20da3
RickyColotl <144842683+RickyColotl@users.noreply.github.com>
2023-11-20 16:22:30 -0500

    Updated for requirements

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
 window.onload = function() {
+    var cookieValue = getCookie("userCookie");
+    if (!cookieValue) {
+        var userInput = prompt("Please enter a value for the cookie:", "");
+        if (userInput != null && userInput !== "") {
+            setCookie("userCookie", userInput, 1);
+        }
+    }
+    console.log(cookieValue);
     displayCookie();
 };
 
@@ -10,7 +18,6 @@ function setCookie(name, value, days) {
         expires = "; expires=" + date.toUTCString();
     }
     document.cookie = name + "=" + (value || "") + expires + "; path=/";
-    displayCookie();
 }
 
 function getCookie(name) {
@@ -19,13 +26,15 @@ function getCookie(name) {
     for(var i = 0; i < ca.length; i++) {
         var c = ca[i];
         while (c.charAt(0) === ' ') c = c.substring(1, c.length);
-        if (c.indexOf(nameEQ) === 0) return c.substring(nameEQ.length, c.length);
+        if (c.indexOf(nameEQ) === 0){
+            return c.substring(nameEQ.length, c.length);
+        }
     }
     return null;
 }
 
 function displayCookie() {
-    var cookieValue = getCookie("testCookie");
+    var cookieValue = getCookie("userCookie");
     if (cookieValue) {
         document.getElementById("cookieDisplay").innerText = "Cookie Value: " + cookieValue;
     } else {
